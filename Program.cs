01f2f78a24b0f2b7293b6574c7556a067d38e61c
sergei2679 <60327654+sergei2679@users.noreply.github.com>
2020-01-30 05:06:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time copyTime = new Time(DateTime.Now);
+            Console.Write(copyTime.Hour + ":" + copyTime.Minute + ":" + copyTime.Second);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,69 @@
+﻿namespace Classes_3
+{
+    class Time
+    {
+        public int Hour { get; private set; }
+        public int Minute { get; private set; }
+        public int Second { get; private set; }
+
+        public Time(int hour, int minute, int second)
+        {
+            if (hour < 0 || hour > 23)
+                Hour = 0;
+            else
+                Hour = hour;
+
+            if (minute < 0 || minute > 59)
+                Minute = 0;
+            else
+                Minute = minute;
+
+            if (second < 0 || second > 59)
+                Second = 0;
+            else
+                Second = second;
+        }
+
+        public Time(System.DateTime existingTimeObject)    // copy constructor
+        {
+            Hour = existingTimeObject.Hour;
+            Minute = existingTimeObject.Minute;
+            Second = existingTimeObject.Second;
+        }
+
+        public int GetHour()
+        {
+            return Hour;
+        }
+        public void SetHour(int hour)
+        {
+            Hour = hour;
+        }
+
+        public int GetMinute()
+        {
+            return Minute;
+        }
+        public void SetMinute(int minute)
+        {
+            Minute = minute;
+        }
+
+        public int GetSecond()
+        {
+            return Hour;
+        }
+        public void SetSecond(int second)
+        {
+            Second = second;
+        }
+
+        public string toString(int hour, int minute, int second)
+        {
+            return "Current Time : " +
+                    hour + ":" + minute + ":" + second;
+
+        }
+    }
+
+}
