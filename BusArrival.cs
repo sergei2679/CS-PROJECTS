3f6be57643398d9aa2cbacbaf94e036d46254479
sergei2679 <60327654+sergei2679@users.noreply.github.com>
2020-02-08 18:55:50 +0200

    Add files via upload

---
+++ BusArrival.cs
@@ -0,0 +1,233 @@
+﻿
+namespace Lesson_20200205_bus_arrival
+{
+    class BusArrival
+    {
+        private const byte maxPassengersNumber = 50;
+        private byte lineNumber, passengersNumber;
+        private Time time;
+
+        public BusArrival(int hour, int minute, int second, byte lineNumber, byte passengersNumber)
+        {
+            this.lineNumber = 0;
+            this.passengersNumber = 0;
+            time = new Time(hour, minute, second);
+            SetLineNumber(lineNumber);
+            SetPassengersNumber(passengersNumber);
+        }
+
+        // copy constructor
+        // public BusArrival(BusArrival copy) : this(copy.hour, copy.minute, copy.second, copy.lineNumber, copy.passengersNumber) { } 
+
+
+        public BusArrival(BusArrival other)       // copy constructor
+        {          
+            lineNumber = other.lineNumber;
+            passengersNumber = other.passengersNumber;
+            time = new Time(other.time);
+        }
+
+        public byte GetLineNumber()
+        {
+            return lineNumber;
+        }
+
+        public void SetLineNumber(byte lineNumber)
+        {
+            this.lineNumber = lineNumber;
+        }
+
+        public byte GetPassengersNumber()
+        {
+            return passengersNumber;
+        }
+
+        public void SetPassengersNumber(byte passengersNumber)
+        {
+            this.passengersNumber = passengersNumber;
+        }
+
+        static string AddZero(int num)    //  static function = no using any variables of class (hour,minute,second)
+        {
+            return (num < 10 ? "0" : "") + num;
+        }
+
+        private static int comparePassengers(byte passengersNumber,byte otherPassengersNumber)
+        {
+            if (passengersNumber < otherPassengersNumber)
+                return otherPassengersNumber - passengersNumber;
+            else
+                return passengersNumber - otherPassengersNumber;
+        }
+
+        public string Fuller(BusArrival other)
+        {
+            int result = comparePassengers(passengersNumber, other.passengersNumber);
+
+            if (passengersNumber < other.passengersNumber)
+                return "  Sender bus is more fuller than This \n " +
+                       "\nsender.passengersNumber : " + other.passengersNumber +
+                       "\nthis.passengersNumber   : " + passengersNumber +
+                       "\nresult                  : " + result;
+
+            else
+                return "  This bus is more fuller than Sender \n " +
+                       "\nsender.passengersNumber : " + other.passengersNumber +
+                       "\nthis.passengersNumber   : " + passengersNumber +
+                       "\nresult                  : " + result;
+        }
+
+        public bool Equals(BusArrival other)
+        {
+            if (other == null)
+                return false;
+            if (this == other)
+                return true;
+            return time.GetHour() == other.time.GetHour()
+                && time.GetMinute() == other.time.GetMinute()
+                && time.GetSecond() == other.time.GetSecond();
+        }
+
+        public bool Before(BusArrival other)
+        {
+            if (time.GetHour() < other.time.GetHour())
+                return true;
+            else if (time.GetMinute() < other.time.GetMinute())
+                return true;
+            else if (time.GetSecond() < other.time.GetSecond())
+                return true;
+            else
+                return false;
+
+            //return time.GetHour() == other.time.GetHour() && time.GetMinute() == other.time.GetMinute() && time.GetSecond() == other.time.GetSecond();
+        }
+
+        public bool isFull(BusArrival busArr)
+        {
+            if (busArr.GetPassengersNumber() == maxPassengersNumber)
+                return true;
+            return false;
+        }
+        private static BusArrival GetBusWith10Passengers(BusArrival[] busArr)
+        {
+            int minIndex = 0;
+
+            for (int i = 0; i < busArr.Length; i++)
+            {
+                if (busArr[i].passengersNumber >= 10 && busArr[i].Before(busArr[minIndex]))
+                {
+                    minIndex = i;
+                }
+            }
+            return busArr[minIndex];
+        }
+
+        public string printBusWith10Passengers(BusArrival[] busArr)
+        {
+            BusArrival bus = GetBusWith10Passengers(busArr);
+
+            return "\n time arrival      | " + AddZero(bus.time.GetHour()) + " : " + AddZero(bus.time.GetMinute()) + " : " + AddZero(bus.time.GetSecond()) +
+                   "\n line number       | " + bus.GetLineNumber() +
+                   "\n passengers number | " + bus.GetPassengersNumber();
+        }
+
+        public string toString()
+        {
+            return
+                    "\n Hour             : " + AddZero(time.GetHour()) +
+                    "\n Minute           : " + AddZero(time.GetMinute()) +
+                    "\n Second           : " + AddZero(time.GetSecond()) +
+                    "\n Line Number      : " + lineNumber +
+                    "\n passengersNumber : " + passengersNumber;
+        }
+    }
+}
+
+
+
+
+// -------------------------------------  bus_arrival with property  ------------- not ended ------------
+// -------------------------------------------------------------------------------------------------------------------
+
+namespace Lesson_20200205_bus_arrival_property
+{
+    class BusArrival
+    {
+        private byte lineNumber, passengersNumber;
+        private Time time;
+
+        public BusArrival(int hour, int minute, int second, byte lineNumber, byte passengersNumber)
+        {
+            LineNumber = lineNumber;
+            PassengersNumber = passengersNumber;
+            time = new Time(hour, minute, second);
+        }
+
+        // copy constructor
+        // public BusArrival(BusArrival copy) : this(copy.hour, copy.minute, copy.second, copy.lineNumber, copy.passengersNumber) { } 
+
+        public BusArrival(BusArrival copy)       // copy constructor
+        {
+            lineNumber = copy.lineNumber;
+            passengersNumber = copy.passengersNumber;
+            time = new Time(copy.time);
+        }
+
+        public byte LineNumber
+        {
+            get
+            {
+                return lineNumber;
+            }
+            set
+            {
+                if (value >= 0)
+                    lineNumber = value;
+            }
+        }
+
+        public byte PassengersNumber
+        {
+            get
+            {
+                return passengersNumber;
+            }
+            set
+            {
+                if (value >= 0)
+                    passengersNumber = value;
+            }
+        }
+
+        static string AddZero(int num)    //  static function = no using any variables of class (hour,minute,second)
+        {
+            return (num < 10 ? "0" : "") + num;
+        }
+
+        public bool Equals(BusArrival other)
+        {
+            if (other == null)
+                return false;
+            if (this == other)
+                return true;
+            return time.GetHour() == other.time.GetHour()
+                && time.GetMinute() == other.time.GetMinute()
+                && time.GetSecond() == other.time.GetSecond();
+        }
+
+        //public bool Before(BusArrival busArrival)
+        //{
+        //    return hour == busArrival.hour && minute == busArrival.minute && second == busArrival.second;
+        //}
+
+        public string toString()
+        {
+            return
+                    "\n Hour             : " + AddZero(time.GetHour()) +
+                    "\n Minute           : " + AddZero(time.GetMinute()) +
+                    "\n Second           : " + AddZero(time.GetSecond()) +
+                    "\n Line Number      : " + lineNumber +
+                    "\n passengersNumber : " + passengersNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Lesson_20200205_bus_arrival
+{
+    class Program
+    {
+        static void Main()
+        {
+            Time time = new Time(10, 30, 0);
+            Time otherTime = new Time(10, 30, 40);
+
+            // this
+            Console.WriteLine("---- THIS BUS ARRIVAL");
+            BusArrival bus = new BusArrival(time.GetHour(), time.GetMinute(), time.GetSecond(), 90, 15);
+            Console.WriteLine(bus.toString());
+
+
+            // other
+            Console.WriteLine("\n ---- OTHER BUS ARRIVAL");
+            BusArrival otherBusArrival = new BusArrival(otherTime.GetHour(), otherTime.GetMinute(), otherTime.GetSecond(), 95, 5);
+            Console.WriteLine(otherBusArrival.toString());
+
+
+            // check passengers
+            Console.WriteLine(" \n\n ------------ CHECK IF BUS SENDER AS PARAMETER IS MORE FULLER -------------------");
+            Console.WriteLine(bus.Fuller(otherBusArrival));
+
+
+            // check time arrival
+            Console.WriteLine(" \n\n ------------ CHECK IF THIS BUS ARRIVAL TIME BEFORE THAN OTHER -------------------");
+
+            if (bus.Before(otherBusArrival) == true)
+                Console.WriteLine("this bus arrival time before than other");
+            else
+                Console.WriteLine("this bus arrival time not before than other");
+
+
+            // check earliest arrival time
+            Console.WriteLine(" \n\n ------------ CHECK EARLIEST ARRIVAL TIME WITH AT LEAST 10 PASSENGERS -------------------");
+
+            var busArr = new BusArrival[]
+            {
+                new BusArrival(10, 23, 20, 90, 30),
+                new BusArrival(10, 20, 20, 90, 20),
+                new BusArrival(08, 23, 20, 90, 15),
+                new BusArrival(09, 23, 20, 90, 30)
+            };
+
+            Console.WriteLine(bus.printBusWith10Passengers(busArr));
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+//namespace Lesson_20200205_bus_arrival_property
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Time time = new Time(10, 30, 0);
+//            Time otherTime = new Time(12, 30, 40);
+
+//            BusArrival bus = new BusArrival(time.GetHour(), time.GetMinute(), time.GetSecond(), 95, 15);
+//            Console.WriteLine(bus.toString());
+
+//            BusArrival copy = new BusArrival(otherTime.GetHour(), otherTime.GetMinute(), otherTime.GetSecond(), 95, 15);
+
+//            Console.WriteLine(copy.toString());
+
+
+//            Console.ReadLine();
+
+//        }
+//    }
+//}
